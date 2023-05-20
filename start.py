2f354116d3e07de811f35791bd43645ddd041efc
WebPRO-Sys <134128554+WebPRO-Sys@users.noreply.github.com>
2023-05-20 10:02:28 +0300

    Create start.py

---
+++ start.py
@@ -0,0 +1,169 @@
+import os
+import time
+import socket, random
+socket_package_id = "1946293649175"
+hashcath = "oen82-ndMe63nd-ndmKr73-nd"
+
+UserAgent = random.choice(["Mozilla/5.0 (iPhone; CPU iPhone OS 11_2_8; like Mac OS X) AppleWebKit/534.39 (KHTML, like Gecko)  Chrome/54.0.2270.327 Mobile Safari/603.7",
+"Mozilla/5.0 (Windows; U; Windows NT 10.4; WOW64) Gecko/20100101 Firefox/63.4",
+"Mozilla/5.0 (Windows; Windows NT 6.2; WOW64; en-US) Gecko/20130401 Firefox/60.6",
+"Mozilla/5.0 (iPhone; CPU iPhone OS 10_8_7; like Mac OS X) AppleWebKit/533.14 (KHTML, like Gecko)  Chrome/55.0.3531.292 Mobile Safari/603.4",
+"Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_9_9) Gecko/20130401 Firefox/59.7",
+"Mozilla/5.0 (U; Linux x86_64; en-US) Gecko/20100101 Firefox/47.9",
+"Mozilla/5.0 (iPhone; CPU iPhone OS 8_7_3; like Mac OS X) AppleWebKit/603.9 (KHTML, like Gecko)  Chrome/51.0.2774.387 Mobile Safari/535.3",
+"Mozilla/5.0 (Linux; U; Android 5.0; Nokia 1100 wifi Build/GRK39F) AppleWebKit/536.26 (KHTML, like Gecko)  Chrome/54.0.2157.187 Mobile Safari/600.5",
+"Mozilla/5.0 (Linux; U; Android 5.1.1; Nexus 6 Build/LMY48B) AppleWebKit/536.45 (KHTML, like Gecko)  Chrome/53.0.3560.318 Mobile Safari/535.3",
+"Mozilla/5.0 (Linux; U; Android 4.4; SAMSUNG SM-E500L Build/KTU84P) AppleWebKit/602.28 (KHTML, like Gecko)  Chrome/50.0.2446.158 Mobile Safari/534.4"])
+
+os.system("clear")
+print ("\033[32mWebsite Bom v1.0\n\033[0m")
+print ("FPS: "+str(random.randint(50, 150))+" PİNG: "+str(random.randint(0, 50))+" CPU°: "+str(random.randint(0, 100))+"%")
+print ("Starting System...")
+time.sleep(1)
+os.system("clear")
+print ("\033[32mWebsite Bom v1.0\n\033[0m")
+print ("FPS: "+str(random.randint(50, 150))+" PİNG: "+str(random.randint(0, 50))+" CPU°: "+str(random.randint(0, 100))+"%")
+print ("Starting System...")
+time.sleep(1)
+os.system("clear")
+print ("\033[32mWebsite Bom v1.0\n\033[0m")
+print ("FPS: "+str(random.randint(50, 150))+" PİNG: "+str(random.randint(0, 50))+" CPU°: "+str(random.randint(0, 100))+"%")
+print ("Starting System...")
+time.sleep(1)
+os.system("clear")
+print ("\033[32mWebsite Bom v1.0\n\033[0m")
+print ("FPS: "+str(random.randint(50, 150))+" PİNG: "+str(random.randint(0, 50))+" CPU°: "+str(random.randint(0, 100))+"%")
+print ("Device System Loading...")
+time.sleep(1)
+os.system("clear")
+print ("\033[32mWebsite Bom v1.0\n\033[0m")
+print ("FPS: "+str(random.randint(50, 150))+" PİNG: "+str(random.randint(0, 50))+" CPU°: "+str(random.randint(0, 100))+"%")
+print ("Device System Loading...")
+time.sleep(1)
+os.system("clear")
+print ("\033[32mWebsite Bom v1.0\n\033[0m")
+print ("FPS: "+str(random.randint(50, 150))+" PİNG: "+str(random.randint(0, 50))+" CPU°: "+str(random.randint(0, 100))+"%")
+print ("Device System Loading...")
+time.sleep(1)
+os.system("clear")
+print ("\033[32mWebsite Bom v1.0\n\033[0m")
+print ("FPS: "+str(random.randint(50, 150))+" PİNG: "+str(random.randint(0, 50))+" CPU°: "+str(random.randint(0, 100))+"%")
+print ("Socket Packages Loading...")
+time.sleep(1)
+os.system("clear")
+print ("\033[32mWebsite Bom v1.0\n\033[0m")
+print ("FPS: "+str(random.randint(50, 150))+" PİNG: "+str(random.randint(0, 50))+" CPU°: "+str(random.randint(0, 100))+"%")
+print ("Socket Packages Loading...")
+time.sleep(1)
+os.system("clear")
+print ("\033[32mWebsite Bom v1.0\n\033[0m")
+print ("FPS: "+str(random.randint(50, 150))+" PİNG: "+str(random.randint(0, 50))+" CPU°: "+str(random.randint(0, 100))+"%")
+print ("Socket Packages Loading...")
+time.sleep(1)
+os.system("clear")
+print ("\033[32mWebsite Bom v1.0\n\033[0m")
+print ("FPS: "+str(random.randint(50, 150))+" PİNG: "+str(random.randint(0, 50))+" CPU°: "+str(random.randint(0, 100))+"%")
+print ("Socket Packages Loading...")
+time.sleep(1)
+os.system("clear")
+print ("\033[32mWebsite Bom v1.0\n\033[0m")
+print ("FPS: "+str(random.randint(50, 150))+" PİNG: "+str(random.randint(0, 50))+" CPU°: "+str(random.randint(0, 100))+"%")
+print ("Socket Packages Loading...")
+time.sleep(1)
+os.system("clear")
+print ("\033[32mWebsite Bom v1.0\n\033[0m")
+print ("FPS: "+str(random.randint(50, 150))+" PİNG: "+str(random.randint(0, 50))+" CPU°: "+str(random.randint(0, 100))+"%")
+print ("Setup of panel...")
+time.sleep(1)
+os.system("clear")
+print ("\033[32mWebsite Bom v1.0\n\033[0m")
+print ("FPS: "+str(random.randint(50, 150))+" PİNG: "+str(random.randint(0, 50))+" CPU°: "+str(random.randint(0, 100))+"%")
+print ("Setup of panel...")
+time.sleep(1)
+os.system("clear")
+print ("\033[32mWebsite Bom v1.0\n\033[0m")
+print ("FPS: "+str(random.randint(50, 150))+" PİNG: "+str(random.randint(0, 50))+" CPU°: "+str(random.randint(0, 100))+"%")
+print ("Setup of panel...")
+time.sleep(1)
+os.system("clear")
+print ("\033[32mWebsite Bom v1.0\n\033[0m")
+print ("FPS: "+str(random.randint(50, 150))+" PİNG: "+str(random.randint(0, 50))+" CPU°: "+str(random.randint(0, 100))+"%")
+print ("Starting...")
+time.sleep(1)
+os.system("clear")
+print ("\033[32mWebsite Bom v1.0\n\033[0m")
+print ("FPS: "+str(random.randint(50, 150))+" PİNG: "+str(random.randint(0, 50))+" CPU°: "+str(random.randint(0, 100))+"%")
+print ("Starting...")
+time.sleep(1)
+os.system("clear")
+print ("\033[32mWebsite Bom v1.0\n\033[0m")
+print ("FPS: "+str(random.randint(50, 150))+" PİNG: "+str(random.randint(0, 50))+" CPU°: "+str(random.randint(0, 100))+"%")
+print ("Starting...")
+time.sleep(1)
+os.system("clear")
+print ("\033[32mWebsite Bom v1.0\n\033[0m")
+print ("FPS: "+str(random.randint(50, 150))+" PİNG: "+str(random.randint(0, 50))+" CPU°: "+str(random.randint(0, 100))+"%")
+print ("Starting...")
+time.sleep(1)
+os.system("clear")
+print ("\033[32mWebsite Bom v1.0\n\033[0m")
+print ("FPS: "+str(random.randint(50, 150))+" PİNG: "+str(random.randint(0, 50))+" CPU°: "+str(random.randint(0, 100))+"%")
+print ("Starting...")
+time.sleep(1)
+os.system("clear")
+print ("\033[32mWebsite Bom v1.0\n\033[0m")
+print ("FPS: "+str(random.randint(50, 150))+" PİNG: "+str(random.randint(0, 50))+" CPU°: "+str(random.randint(0, 100))+"%")
+print ("Starting...")
+time.sleep(1)
+os.system("clear")
+print ("\033[32mWebsite Bom v1.0\n\033[0m")
+soc = input("Website/Page/IP/URL: ")
+if "https://" in soc:
+    os.system("clear")
+    test = input("Are you trying to blow up Website? or page? Y/N: ")
+    if "n" in test:
+        os.system("clear")
+    if "N" in test:
+        os.system("clear")
+if "." in soc:
+    os.system("clear")
+    test = input("Are you trying to blow up IP Adress? Y/N: ")
+    if "n" in test:
+        os.system("clear")
+    if "N" in test:
+        os.system("clear")
+else:
+    print ("This is not IP/URL/PAGE Error")
+    os.system("exit")
+os.system("clear")
+yesno = input('Are you sure you want to blow this "'+soc+'"? Y/N: ')
+if "n" in yesno:
+    os.system("exit")
+elif "N" in yesno:
+    os.system("exit")
+elif "y" in yesno:
+    os.system("clear")
+elif "y" in yesno:
+    os.system("clear")
+else:
+    print ("Invalid Option ×")
+    os.system("exit")
+os.system("clear")
+num = 1
+while True:
+    byte = random.randint(50000, 100000)
+    duty = random.choice(["SQL-data","Json-Ex","WindowsGo","rubrub45","acess-sys","str-to","int-to","Toint32","Toint64","change-value","change-str","block","Change-To-SYS","DDoS-Attack","DoS-PrO","Musical-Change","Mp3-Change","Mp4-Change","Json-Change","FuckTheSys","CHANGE-TEXTS","CHANGE_FİLESNAME","Software-BlowUP","AzureDat-Change"])
+    center = random.choice(["SOFTWARE","PY","JS","JSON","DATA","SQL","CSS","C#","AZUR","MOZİLLA","USERS","REPEAT","RUBY","SYS","OS","ADMİNS","SUPERUSER-ROOT","ROOTFS","WALL","MARK","WEB"])
+    color = "\033[32m"
+    print (color+"Package Send ID:"+str(num))
+    print ("BYTE: "+str(byte))
+    print ("DUTY: "+duty)
+    print ("CENTER: "+center)
+    print ("TO: "+soc)
+    print ("\033[0m")
+    num += 1
+    time.sleep(random.choice([0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1]))
+
+
+
+
